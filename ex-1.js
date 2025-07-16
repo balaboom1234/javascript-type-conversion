589f9ce42081b4beae838a75280d85d08e439290
Thanisorn <thanisorn.sir@gmail.com>
2025-07-16 14:30:29 +0700

    feat: fix a bug

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,11 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
+depositAmountFromUser = Number("10");
+
 accountBalance = accountBalance + depositAmountFromUser;
+
+console.log(accountBalance)
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
 
